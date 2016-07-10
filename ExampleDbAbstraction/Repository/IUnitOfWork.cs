59ed1da72fd0da125d6105fab0bf9c0e306ee375
David McCullough <damccull@gmail.com>
2016-07-10 00:40:19 -0400

    Add unit of work pattern.

---
+++ ExampleDbAbstraction/Repository/IUnitOfWork.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExampleDbAbstraction.Repository {
+    public interface IUnitOfWork : IDisposable {
+        IFlightRepository Flights { get; }
+        IAircraftRepository Aircraft { get; }
+        int Complete();
+    }
+}
---
+++ ExampleDbAbstraction/Repository/UnitOfWork.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExampleDbAbstraction.Repository {
+    public class UnitOfWork : IUnitOfWork {
+        private readonly ApplicationDbContext db;
+
+        public IFlightRepository Flights { get; private set; }
+        public IAircraftRepository Aircraft { get; private set; }
+
+        public UnitOfWork(ApplicationDbContext context) {
+            this.db = context;
+            this.Flights = new FlightRespository(context);
+            this.Aircraft = new AircraftRespository(context);
+        }
+
+        public int Complete() {
+            return db.SaveChanges();
+        }
+
+        public void Dispose() {
+            db.Dispose();
+        }
+    }
+}
